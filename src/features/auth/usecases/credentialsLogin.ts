55ef9b8a71fe2c1584ed347d865b442286a7799c
alcrro <alex.roventa94@gmail.com>
2026-01-31 22:45:12 +0200

    supabase trigger new table

--- src/features/auth/usecases/credentialsLogin.ts
+++ src/features/auth/usecases/credentialsLogin.ts
@@ -1,11 +1,30 @@
+import { supabase } from "../../../shared/libs/supabase/supabaseinsta";
+import { getCurrentDeviceInfo } from "../services/getCurrentDeviceInfo";
 import { supabaseCredentialsLogin } from "../services/supabaseCredentialsLogin";
 import type { AuthCredentials } from "./authEngine";
 
 export async function credentialsLogin(payload: AuthCredentials) {
   try {
+    const { os, deviceType, browser } = getCurrentDeviceInfo();
+
     const { data, error } = await supabaseCredentialsLogin(payload);
 
     if (error) throw error;
+
+    // sanity check
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+      const { error: errorHandler } = await supabase.rpc("record_login_audit", {
+        device_type: deviceType,
+      });
+
+      if (errorHandler) {
+        throw errorHandler;
+      }
+    }
     return data;
   } catch (error) {
     throw new Error(error instanceof Error ? error.message : "Internal error");
