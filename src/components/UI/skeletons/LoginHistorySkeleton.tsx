ddd7cc118444b10a5e0b68ad44c991f91276644b
alcrro <alex.roventa94@gmail.com>
2026-02-22 18:21:59 +0200

    choange some colors for table

--- src/components/UI/skeletons/LoginHistorySkeleton.tsx
+++ src/components/UI/skeletons/LoginHistorySkeleton.tsx
@@ -10,7 +10,9 @@ const LoginHistorySkeleton = () => {
       </div>
       <TableContainer>
         <table
-          className={"w-full table-fixed border-separate border-spacing-0"}
+          className={
+            "w-full table-fixed border-separate border-spacing-0 bg-(--table-bg) "
+          }
           aria-label={"login history"}
         >
           <thead className={"w-full max-xl:hidden xl:table-header-group"}>
@@ -19,7 +21,7 @@ const LoginHistorySkeleton = () => {
                 <th
                   scope={"col"}
                   className={
-                    "sticky top-0 bg-white/30 backdrop-blur-md text-gray-900 p-3 text-sm font-semibold tracking-wide first:rounded-tl-md first:rounded-bl-md last:rounded-tr-md last:rounded-br-md capitalize"
+                    "sticky top-0 bg-(--table-row) hover:bg-(--table-row-hover) backdrop-blur-md  p-3 text-sm font-semibold tracking-wide first:rounded-tl-md first:rounded-bl-md last:rounded-tr-md last:rounded-br-md capitalize"
                   }
                   data-label={item.label}
                 >
@@ -35,18 +37,18 @@ const LoginHistorySkeleton = () => {
             </tr>
           </thead>
           <tbody className={"tbody"}>
-            <tr className={"max-xl:hidden bg-(--background-container)"}>
+            <tr className={"max-xl:hidden "}>
               <td colSpan={tableDocumentMap.length} className="h-2"></td>
             </tr>
             {Array.from({ length: 5 }).map((_row, i, arr) => (
               <tr
                 key={i}
-                className={`block mb-2 text-transparent xl:table-row bg-white/15 backdrop-blur-lg shadow-lg lg:hover:bg-white/30 w-full  ${i === 0 ? "rounded-tl-md" : ""}`}
+                className={`block mb-2 text-transparent xl:table-row bg-(--table-row) hover:bg-(--table-row-hover)  backdrop-blur-lg shadow-lg lg:hover:bg-white/30 w-full  ${i === 0 ? "rounded-tl-md" : ""}`}
               >
                 {tableDocumentMap.map((h, colIndex) => (
                   <td
                     key={h.key}
-                    className={`p-2 ${h.key !== "ip_address" ? "capitalize" : ""} h-12 flex justify-between items-center xl:table-cell ${i === arr.length - 1 && colIndex === 0 ? "rounded-bl-md" : ""} ${i === arr.length - 1 && colIndex === 5 - 1 ? "rounded-br-md" : ""} ${i === 0 && colIndex === 0 ? "rounded-tl-md" : ""} ${i === 0 && colIndex === tableDocumentMap.length - 1 ? "rounded-tr-md" : ""}`}
+                    className={`p-2 ${h.key !== "ip_address" ? "capitalize" : ""} text-center h-12 flex justify-between items-center xl:table-cell ${i === arr.length - 1 && colIndex === 0 ? "rounded-bl-md" : ""} ${i === arr.length - 1 && colIndex === 5 - 1 ? "rounded-br-md" : ""} ${i === 0 && colIndex === 0 ? "rounded-tl-md" : ""} ${i === 0 && colIndex === tableDocumentMap.length - 1 ? "rounded-tr-md" : ""}`}
                   >
                     <span
                       className={
@@ -69,7 +71,7 @@ const LoginHistorySkeleton = () => {
             {Array.from({ length: 5 }, (_, i) => (
               <tr
                 key={i}
-                className={`block max-xl:hidden xl:table-row nth-of-type-[2]:rounded-t-md last:rounded-b-md max-lg:hidden ${tableDocumentMap.length - 1 === 3 ? "text-red-500" : ""}`}
+                className={`block max-xl:hidden xl:table-row bg-(--table-row) hover:bg-(--table-row-hover)  nth-of-type-[2]:rounded-t-md last:rounded-b-md max-lg:hidden ${tableDocumentMap.length - 1 === 3 ? "text-red-500" : ""}`}
               >
                 {Array.from({ length: tableDocumentMap.length }, (_, i) => (
                   <td
--- src/components/organisms/dashboard/loginHistory/Table.tsx
+++ src/components/organisms/dashboard/loginHistory/Table.tsx
@@ -1,7 +1,4 @@
-import {
-  tableDocumentMap,
-  type TableDocumentMapProps,
-} from "../../../../shared/data/dashboard/loginHistoryData";
+import { type TableDocumentMapProps } from "../../../../shared/data/dashboard/loginHistoryData";
 
 interface TableDataProps<D extends { id: string | number }> {
   dataHeader: TableDocumentMapProps[];
@@ -14,11 +11,17 @@ function Table<D extends { id: string | number }>({
 }: TableDataProps<D>) {
   return (
     <table
-      className={"w-full table-fixed border-separate border-spacing-y-0"}
+      className={
+        "w-full table-fixed border-separate border-spacing-y-0 bg-(--table-bg)"
+      }
       aria-label={"login history"}
     >
-      <thead className={"thead max-xl:hidden xl:table-header-group"}>
-        <tr className="block xl:table-row bg-white/20 hover:bg-white/35 transition">
+      <thead
+        className={
+          "thead max-xl:hidden xl:table-header-group bg-(--table-header)"
+        }
+      >
+        <tr className="block xl:table-row bg-(--table-row) hover:bg-(--table-row-hover) transition">
           {dataHeader.map((item) => (
             <th
               key={item.key}
@@ -33,19 +36,19 @@ function Table<D extends { id: string | number }>({
           ))}
         </tr>
       </thead>
-      <tbody className={"tbody"}>
-        <tr className={"max-xl:hidden bg-(--background-container)"}>
-          <td colSpan={tableDocumentMap.length} className="h-2"></td>
+      <tbody className={"tbody "}>
+        <tr className={"max-xl:hidden"}>
+          <td colSpan={dataHeader.length} className="h-2"></td>
         </tr>
         {dataBody.map((row, i, arr) => (
           <tr
             key={row.id}
-            className="block mb-2 xl:table-row nth-of-type-[2]:rounded-t-md last:rounded-b-md  bg-white/15 backdrop-blur-lg lg:hover:bg-white/30 lg:hover:rounded-md cursor-default max-xl:rounded-md"
+            className=" flex-col gap-2 mb-2 xl:table-row nth-of-type-[2]:rounded-t-md last:rounded-b-md bg-(--table-row) hover:bg-(--table-row-hover) backdrop-blur-lg  lg:hover:rounded-md cursor-default max-xl:rounded-md"
           >
             {dataHeader.map((h, colIndex) => (
               <td
                 key={h.key}
-                className={`p-2 ${h.key !== "ip_address" && "capitalize"} flex justify-between items-center xl:table-cell text-center ${i === arr.length - 1 && colIndex === 0 ? "rounded-bl-md" : ""} ${i === arr.length - 1 && colIndex === dataHeader.length - 1 ? "rounded-br-md" : ""} ${i === 0 && colIndex === 0 ? "rounded-tl-md" : ""} ${i === 0 && colIndex === tableDocumentMap.length - 1 ? "rounded-tr-md" : ""}`}
+                className={`p-2 ${h.key !== "ip_address" ? "capitalize" : ""} flex justify-between items-center xl:table-cell text-center ${i === arr.length - 1 && colIndex === 0 ? "rounded-bl-md" : ""} ${i === arr.length - 1 && colIndex === dataHeader.length - 1 ? "rounded-br-md" : ""} ${i === 0 && colIndex === 0 ? "rounded-tl-md" : ""} ${i === 0 && colIndex === dataHeader.length - 1 ? "rounded-tr-md" : ""}`}
               >
                 <span className={"xl:hidden font-medium max-xl:text-left"}>
                   {h.label}
@@ -62,7 +65,7 @@ function Table<D extends { id: string | number }>({
           Array.from({ length: 10 - dataBody.length }, (_, i) => (
             <tr
               key={i}
-              className={`block max-xl:hidden xl:table-row nth-of-type-[2]:rounded-t-md last:rounded-b-md max-lg:hidden ${dataBody.length - 1 === 3 && "text-red-500"}`}
+              className={` max-xl:hidden xl:table-row nth-of-type-[2]:rounded-t-md  bg-(--table-row) hover:bg-(--table-row-hover) last:rounded-b-md max-lg:hidden ${dataBody.length - 1 === 3 && "text-red-500"}`}
             >
               {Array.from({ length: dataHeader.length }, (_, i) => (
                 <td
